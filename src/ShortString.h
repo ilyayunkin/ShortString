b501f0eb31e05a0fdae3842f38730b01ad7807c4
ilyayunkin <ilya.yunkin@ya.ru>
2020-11-23 08:41:30 +0600

    noexcept added to all function prototypes

--- src/ShortString.h
+++ src/ShortString.h
@@ -13,10 +13,10 @@ template <int len>
 struct BasicShortString
 {
     std::array<char, len> buf;
-    constexpr BasicShortString() : buf({'\0'}){
+    constexpr BasicShortString() noexcept : buf({'\0'}){
         buf[len - 1] = len - 1;
     }
-    constexpr BasicShortString(const char *const s){
+    constexpr BasicShortString(const char *const s) noexcept{
         buf[len - 1] = len - 1;
         auto outp = s;
         auto inp = buf.begin();
@@ -30,40 +30,40 @@ struct BasicShortString
         *inp = '\0';
     };
 
-    constexpr auto capacity()const{return len - 1;}
-    constexpr auto size()const{return capacity() - buf[len - 1];}
-    constexpr auto length()const{return size();}
-    constexpr auto empty()const{return buf[0] == '\0';}
+    constexpr auto capacity()const noexcept{return len - 1;}
+    constexpr auto size()const noexcept{return capacity() - buf[len - 1];}
+    constexpr auto length()const noexcept{return size();}
+    constexpr auto empty()const noexcept{return buf[0] == '\0';}
 
-    constexpr auto begin()const{return buf.begin();}
-    constexpr auto end()const{return buf.begin() + size();}
-    constexpr auto begin(){return buf.begin();}
-    constexpr auto end(){return buf.begin() + size();}
+    constexpr auto begin()const noexcept{return buf.begin();}
+    constexpr auto end()const noexcept{return buf.begin() + size();}
+    constexpr auto begin() noexcept{return buf.begin();}
+    constexpr auto end() noexcept{return buf.begin() + size();}
 
-    constexpr auto cbegin()const{return buf.cbegin();}
-    constexpr auto cend()const{return buf.cbegin() + size();}
+    constexpr auto cbegin()const noexcept{return buf.cbegin();}
+    constexpr auto cend()const noexcept{return buf.cbegin() + size();}
 
-    constexpr auto rbegin()const{return buf.rend() - size();}
-    constexpr auto rend()const{return buf.rend();}
-    constexpr auto rbegin(){return buf.rend() - size();}
-    constexpr auto rend(){return buf.rend();}
+    constexpr auto rbegin()const noexcept{return buf.rend() - size();}
+    constexpr auto rend()const noexcept{return buf.rend();}
+    constexpr auto rbegin() noexcept{return buf.rend() - size();}
+    constexpr auto rend() noexcept{return buf.rend();}
 
-    constexpr auto crbegin()const{return buf.crend() - size();}
-    constexpr auto crend()const{return buf.crend();}
+    constexpr auto crbegin()const noexcept{return buf.crend() - size();}
+    constexpr auto crend()const noexcept{return buf.crend();}
 
-    constexpr operator char*(){return buf.data();}
-    constexpr operator const char*()const{return buf.data();}
+    constexpr operator char*() noexcept{return buf.data();}
+    constexpr operator const char*()const noexcept{return buf.data();}
 
     //Returns a reference to the first element in the container.
     //Calling front on an empty container is undefined behaviour.
-    constexpr auto front()const{return *begin();}
-    constexpr auto front(){return *begin();}
+    constexpr auto front()const noexcept{return *begin();}
+    constexpr auto front() noexcept{return *begin();}
     //Returns a reference to the last element in the container.
     //Calling back on an empty container causes undefined behavior.
-    constexpr auto back()const{return *std::prev(end());}
-    constexpr auto back(){return *std::prev(end());}
+    constexpr auto back()const noexcept{return *std::prev(end());}
+    constexpr auto back() noexcept{return *std::prev(end());}
 
-    void operator=(const char *const s){
+    void operator=(const char *const s) noexcept{
         buf[len - 1] = len - 1;
         auto outp = s;
         auto inp = buf.begin();
@@ -76,7 +76,7 @@ struct BasicShortString
         *inp = '\0';
         assert(inp < buf.end());
     };
-    void operator+=(const char *const s){
+    void operator+=(const char *const s) noexcept{
         auto outp = s;
         auto inp = end();
         while(*outp != 0 && buf[len - 1]){
@@ -88,7 +88,7 @@ struct BasicShortString
         *inp = '\0';
         assert(inp < buf.end());
     };
-    void operator+=(const char c){
+    void operator+=(const char c) noexcept{
         auto inp = end();
         if(buf[len - 1]){
             *inp = c;
@@ -98,20 +98,20 @@ struct BasicShortString
         *inp = '\0';
         assert(inp < buf.end());
     };
-    void push_back(const char *const s){operator+=(s);}
-    void append(const char *const s){operator+=(s);}
-    void push_back(const char c){operator+=(c);}
-    constexpr bool operator ==(const BasicShortString &sr)
+    void push_back(const char *const s) noexcept{operator+=(s);}
+    void append(const char *const s) noexcept{operator+=(s);}
+    void push_back(const char c) noexcept{operator+=(c);}
+    constexpr bool operator ==(const BasicShortString &sr) noexcept
     {
         return !std::strcmp(buf.data(),  sr.buf.data());
     }
-    constexpr bool operator <(const BasicShortString &sr)
+    constexpr bool operator <(const BasicShortString &sr) noexcept
     {
         return std::strcmp(buf.data(),  sr.buf.data()) == -1;
     }
 };
 template <int len>
-constexpr bool operator ==(const BasicShortString<len> &sl, const char *sr)
+constexpr bool operator ==(const BasicShortString<len> &sl, const char *sr) noexcept
 {
     return !std::strcmp(sl,  sr);
 }
